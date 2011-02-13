251381cf0ed2531a5c4ede819f5d05b052359593
Michael van Laar <michael@van-laar.de>
2011-02-13 12:26:12 +0100

    Integration of the updated FOUT prevent script (Paul Irish updated the script to change stylesheet injection method, fixing a FF4 bug.)

--- webfontloadenhancer.js
+++ webfontloadenhancer.js
@@ -283,7 +283,8 @@ EventHelpers.addPageLoadEvent("TypeHelpers.insertClasses");
   var d = document, e = d.documentElement, s = d.createElement('style');
   if ( (e.style.MozTransform === '') || (e.style.OTransform === '') ){
     s.textContent = 'body{visibility:hidden}';
-    e.firstChild.appendChild(s);
+    var r = document.getElementsByTagName('script')[0];
+    r.parentNode.insertBefore(s, r);
     function f(){ s.parentNode && s.parentNode.removeChild(s); }
     addEventListener('load',f,false);
     setTimeout(f,3000); 
